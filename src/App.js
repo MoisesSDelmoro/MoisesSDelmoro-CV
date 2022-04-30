bb4236cda52e3f68fd70c7def7ee83e28dda48c7
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2022-04-30 18:56:13 -0300

    start project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Start Currículo em React JS
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
