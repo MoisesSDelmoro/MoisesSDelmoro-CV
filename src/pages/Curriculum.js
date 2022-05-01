49f33a3aa1877efade65c13dbacfe38ef17ad953
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2022-04-30 21:20:57 -0300

    update data and template

--- src/pages/Curriculum.js
+++ src/pages/Curriculum.js
@@ -2,17 +2,17 @@ import React from "react";
 
 import Global from "../styles/global";
 
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 
 
 const goal = "OBJETIVO"
-const goal_message = "Atuar no desenvolvimento Mobile, colaborando com aequipe para criar soluções inovadoras e orientadas para o desempenho, crescer tanto pessoalmente, quanto profissionalmente."
+const goal_message = "Atuar no desenvolvimento Mobile, colaborando com a equipe para criar soluções inovadoras e orientadas para o desempenho, crescer tanto pessoalmente, quanto profissionalmente."
 
 const contact = "CONTATO"
-const contact_message = "TELEFONE: +55 (35) 999-578-820 EMAIL: moisesdelmoro@outlook.com GITHUB: MoisesSDelmoro"
+const contacts = ["TELEFONE: +55 (35) 999-578-820", "MAIL: moisesdelmoro@outlook.com", "GITHUB: MoisesSDelmoro"]
 
 const skills = "HABILIDADES"
-const skills_message = "Flutter JavaScript React Js Python Django Dart Java C++ HTML CSS MySQL Inglês : Intermediário (Cursando – KNN – Ouro Fino - MG)"
+const my_skills = ["Flutter", "JavaScript", "React Js", "Python", "Django", "Dart", "Inglês : Intermediário (Cursando – KNN – Ouro Fino - MG)"]
 
 const name = "Moises S Delmoro"
 const course = "Bacharel em Engenharia da Computação"
@@ -20,7 +20,7 @@ const course = "Bacharel em Engenharia da Computação"
 const academic_education = "FORMAÇÃO ACADÊMICA"
 
 const university = "INATEL - Instituto Nacional de Telecomunicações";
-const university_message = "[01/2017] – [previsão 12/2021] Cursando o 5º ano (10º período) de engenharia da computação. Disponibilidade 40h/semana."
+const university_message = "[01/2017] – [12/2021] Bacharel em Engenharia da Computação."
 
 const school = "Instituto Federal do Sul de Minas – Câmpus Incofidentes"
 const school_essage = "[01/2014] – [12/2016] Técnico em informática com ênfase em programação e desenvolvimento Web."
@@ -35,37 +35,47 @@ const projects = "PROJETOS"
 function Curriculum() {
   return (
     <>
-      <div className="Page">
-        <Global />
-        {/* <Home boxData={data} /> */}
-        <Grid container style={{ textAlign: "center" }}>
-          <Grid item xs={3} style={{ backgroundColor: "green" }}>
-            <Grid container direction="column" justify="center" alignItems="center">
-              <Grid item>
-                <h2>{goal}</h2>
-                <h5>{goal_message}</h5>
-                <h2>{contact}</h2>
-                <h5>{contact_message}</h5>
-                <h2>{skills}</h2>
-                <h5>{skills_message}</h5>
-              </Grid>
+      <Global />
+      {/* <Home boxData={data} /> */}
+      <Grid container>
+        <Grid item xs={3} style={{ backgroundColor: "#c4c4c4" }}>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <Typography variant="h6">{goal}</Typography>
+              <Typography variant="body2">{goal_message}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">{contact}</Typography>
+              {contacts.map((item) => <Typography variant="body2" key={item}>{item}</Typography>)}
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">{skills}</Typography>
+              {my_skills.map((item) => <Typography variant="body2" key={item}>{item}</Typography>)}
             </Grid>
           </Grid>
-          <Grid item xs={9} style={{ backgroundColor: "red" }}>
-            <h2>{name}</h2>
-                <h5>{course}</h5>
-                <h5>{academic_education}</h5>
-                <h5>{university}</h5>
-                <h5>{university_message}</h5>
-                <h5>{school}</h5>
-                <h5>{school_essage}</h5>
-                <h5>{work_sperience}</h5>
-                <h5>{bedu_tech}</h5>
-                <h5>{bedu_tech_message}</h5>
-                <h5>{projects}</h5>
+        </Grid>
+        <Grid item xs={9} style={{ backgroundColor: "#c5c5c5" }}>
+          <Grid item>
+            <Typography variant="h5">{name}</Typography>
+            <Typography variant="body2">{course}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">{academic_education}</Typography>
+            <Typography variant="body2">{university}</Typography>
+            <Typography variant="body2">{university_message}</Typography>
+            <Typography variant="body2">{school}</Typography>
+            <Typography variant="body2">{school_essage}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">{work_sperience}</Typography>
+            <Typography variant="body2">{bedu_tech}</Typography>
+            <Typography variant="body2">{bedu_tech_message}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2">{projects}</Typography>
           </Grid>
         </Grid>
-      </div>
+      </Grid>
     </>
   );
 }
